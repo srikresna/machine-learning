d98fb8ea11f48899db02650f3c8de90b1f3db593
srikresna <srikresna383@gmail.com>
2024-05-01 20:55:14 +0700

    feat: participant pipeline

---
+++ bahan-ajar-lintasarta/pipeline_tasha_final/local_pipeline.py
@@ -0,0 +1,59 @@
+import os
+from typing import Text
+
+from absl import logging
+from tfx.orchestration import metadata, pipeline
+from tfx.orchestration.beam.beam_dag_runner import BeamDagRunner
+
+PIPELINE_NAME = "customer-churn-pipeline"
+
+# pipeline inputs
+DATA_ROOT = "data"
+TRANSFORM_MODULE_FILE = "modules/customer_churn_transform.py"
+TRAINER_MODULE_FILE = "modules/customer_churn_trainer.py"
+
+# pipeline outputs
+OUTPUT_BASE = "output"
+serving_model_dir = os.path.join(OUTPUT_BASE, 'serving_model')
+pipeline_root = os.path.join(OUTPUT_BASE, PIPELINE_NAME)
+metadata_path = os.path.join(pipeline_root, "metadata.sqlite")
+
+def init_local_pipeline(
+    components, pipeline_root: Text
+) -> pipeline.Pipeline:
+
+    logging.info(f"Pipeline root set to: {pipeline_root}")
+    beam_args = [
+        "--direct_running_mode=multi_processing"
+        # 0 auto-detect based on on the number of CPUs available 
+        # during execution time.
+        "----direct_num_workers=0" 
+    ]
+
+    return pipeline.Pipeline(
+        pipeline_name=PIPELINE_NAME,
+        pipeline_root=pipeline_root,
+        components=components,
+        enable_cache=True,
+        metadata_connection_config=metadata.sqlite_metadata_connection_config(
+            metadata_path
+        ),
+        eam_pipeline_args=beam_args
+    )
+
+if __name__ == "__main__":
+    logging.set_verbosity(logging.INFO)
+
+    from modules.components import init_components
+
+    components = init_components(
+        DATA_ROOT,
+        training_module=TRAINER_MODULE_FILE,
+        transform_module=TRANSFORM_MODULE_FILE,
+        training_steps=5000,
+        eval_steps=1000,
+        serving_model_dir=serving_model_dir,
+    )
+
+    pipeline = init_local_pipeline(components, pipeline_root)
+    BeamDagRunner().run(pipeline=pipeline)
---
+++ bahan-ajar-lintasarta/pipeline_tasha_final/modules/customer_churn_transform.py
@@ -0,0 +1,67 @@
+"""Transform module
+"""
+
+import tensorflow as tf
+import tensorflow_transform as tft
+
+CATEGORICAL_FEATURES = {
+    "InternetService": 3,
+    "SeniorCitizen": 2,
+    "PaperlessBilling": 2,
+    "Partner": 2,
+    "PhoneService": 2,
+    "StreamingTV": 3,
+    "gender": 2
+}
+NUMERICAL_FEATURES = [
+    "MonthlyCharges",
+    "TotalCharges",
+    "tenure"
+]
+LABEL_KEY = "Churn"
+
+def transformed_name(key):
+    """Renaming transformed features"""
+    return key + "_xf"
+
+def convert_num_to_one_hot(label_tensor, num_labels=2):
+    """
+    Convert a label (0 or 1) into a one-hot vector
+    Args:
+        int: label_tensor (0 or 1)
+    Returns
+        label tensor
+    """
+    one_hot_tensor = tf.one_hot(label_tensor, num_labels)
+    return tf.reshape(one_hot_tensor, [-1, num_labels])
+
+
+def preprocessing_fn(inputs):
+    """
+    Preprocess input features into transformed features
+
+    Args:
+        inputs: map from feature keys to raw features.
+
+    Return:
+        outputs: map from feature keys to transformed features.
+    """
+
+    outputs = {}
+
+    for key in CATEGORICAL_FEATURES:
+        dim = CATEGORICAL_FEATURES[key]
+        int_value = tft.compute_and_apply_vocabulary(
+            inputs[key], top_k=dim + 1
+        )
+        outputs[transformed_name(key)] = convert_num_to_one_hot(
+            int_value, num_labels=dim + 1
+        )
+
+    for feature in NUMERICAL_FEATURES:
+        outputs[transformed_name(feature)] = tft.scale_to_0_1(inputs[feature])
+
+
+    outputs[transformed_name(LABEL_KEY)] = tf.cast(inputs[LABEL_KEY], tf.int64)
+
+    return outputs
